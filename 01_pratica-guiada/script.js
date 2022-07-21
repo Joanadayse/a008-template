c630a02d658b1c8cc9a3519833f2c291a46154be
Joanadayse <daysejoana3@gmail.com>
2022-07-21 11:03:13 -0300

    aula8

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,16 @@
-//CODIGO AQUI
+// CÓDIGO AQUI
+// Se eu tenho 15 reais, e recebo mais 22.40. Quanto dinheiro tenho agora?
+let tenho =15 + 22.40
+console.log(tenho)
+// Gastei 12.34. E agora, qual é o meu saldo?
+tenho = tenho - 12.34
+console.log(tenho)
+// Precisei dividir o dinheiro igualmente com meus dois irmãos. Quanto ficou pra cada um?
+tenho = tenho / 3
+console.log(tenho)
+// Apliquei minha parte do dinheiro, e ele rendeu até ficar 3.42 vezes maior. Quanto vale agora?
+tenho = tenho * 3.42
+console.log(tenho)
+// Reparti o dinheiro em fatias inteiras entre três contas, e deixei o resto para comprar um chicletinho. Quanto dinheiro sobrou para o chiclete?
+tenho = tenho % 3
+console.log(tenho)
